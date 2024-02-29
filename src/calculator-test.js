5f79b4c19879afc2b547ca1fa1db9f3a592b69f4
root <root@Emmett>
2024-02-29 12:56:10 -0800

    added tests for loan calculator

--- src/calculator-test.js
+++ src/calculator-test.js
@@ -1,11 +1,27 @@
+describe("calculate monthly rate with positives", function () {
+  it("should calculate the monthly rate correctly", function () {
+    expect(
+      calculateMonthlyPayment({ amount: 17000, years: 10, rate: 4.8 })
+    ).toEqual("178.65");
+  });
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+  it("should return a result with 2 decimal places", function () {
+    expect(
+      calculateMonthlyPayment({ amount: 10043, years: 8, rate: 5.8 })
+    ).toEqual("131.00");
+  });
 });
 
+describe("calculate monthly rate with negatives", function () {
+  it("should calculate the monthly rate correctly with negative interest", function () {
+    expect(
+      calculateMonthlyPayment({ amount: 17000, years: 10, rate: -4.8 })
+    ).toEqual("110.10");
+  });
 
-it("should return a result with 2 decimal places", function() {
-  // ..
+  it("should calculate the monthly rate correctly with negative years", function () {
+    expect(
+      calculateMonthlyPayment({ amount: 17000, years: -10, rate: 4.8 })
+    ).toEqual("-110.65");
+  });
 });
-
-/// etc
